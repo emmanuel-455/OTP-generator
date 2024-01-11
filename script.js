0b1a6a37ddbc761c3fc5f9bf9ba191e401ea86da
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-01-12 00:50:36 +0100

    update

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 let generateOTP = () => {
-    //Define the length of the OTP
     const otpLength = 6;
   
-    // Generate a random numeric OTP with exactly 6 digits
     const otp = Math.floor(100000 + Math.random() * 900000);
-  
-    //Display the generated OTP
     document.getElementById("otpDisplay").innerText = `${otp}`;
   };
   
